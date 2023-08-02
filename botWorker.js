68811fd769f893f4bb130681a61f38835250fb44
MysticalShadoow <86856145+MysticalShadoow@users.noreply.github.com>
2023-08-02 10:39:56 -0700

    Add files via upload

---
+++ botWorker.js
@@ -0,0 +1,38 @@
+process.on('uncaughtException', (err) => {
+  console.error('Unhandled error:', err.stack || err.toString());
+  process.exit(1); // Terminate the process to avoid undefined behavior
+});
+
+// botWorker.js
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const mineflayer = require('mineflayer');
+
+function initBot() {
+  try {
+    const { username, host, port, version } = workerData;
+    const bot = mineflayer.createBot({
+      host,
+      port,
+      username,
+      version,
+    });
+
+    bot.once('spawn', () => {
+      //parentPort.postMessage({ type: 'botReady', bot });
+    });
+
+    bot.on('error', (error) => {
+      parentPort.postMessage({ type: 'error', error: error.toString() });
+    });
+
+    bot.on('end', () => {
+      parentPort.postMessage({ type: 'error', error: 'Bot disconnected unexpectedly.' });
+    });
+  } catch (error) {
+    parentPort.postMessage({ type: 'error', error: error.stack || error.message });
+  }
+}
+
+if (!isMainThread) {
+  initBot();
+}
---
+++ index.js
@@ -0,0 +1,90 @@
+process.on('uncaughtException', (err) => {
+  console.error('Unhandled error:', err.stack || err.toString());
+  process.exit(1); // Terminate the process to avoid undefined behavior
+});
+
+// index.js
+const { Worker } = require('worker_threads');
+const mineflayer = require('mineflayer');
+
+// Settings
+const botArgs = {
+  host: 'NubPlayzBoi.aternos.me',
+  port: 26216,
+  version: '1.18.2'
+};
+
+const NUM_BOTS = 4; // Number of bot instances
+const SPAWN_DELAY = 5000; 
+
+class MCBot {
+  constructor(username) {
+    this.username = username;
+    this.host = botArgs.host;
+    this.port = botArgs.port;
+    this.version = botArgs.version;
+
+    this.initBot();
+  }
+
+  initBot() {
+    this.worker = new Worker('./botWorker.js', {
+      workerData: {
+        username: this.username,
+        host: this.host,
+        port: this.port,
+        version: this.version,
+      },
+    });
+
+    this.worker.on('message', (message) => {
+      if (message.type === 'botReady') {
+        this.bot = message.bot;
+        this.initEvents();
+        console.log(`[${this.username}] Worker initialized`);
+      } else if (message.type === 'error') {
+        console.error(`[${this.username}] Worker encountered an error:`, message.error);
+      }
+    });
+  }
+
+  initEvents() {
+    if (!this.bot) {
+      return;
+    }
+
+    // ... Rest of your event handling code ...
+
+    // Handle bot disconnection
+    this.bot.on('end', (reason) => {
+      console.log(`[${this.username}] Disconnected: ${reason}`);
+      this.reconnect();
+    });
+  }
+
+  reconnect() {
+    if (this.worker) {
+      this.worker.terminate();
+    }
+    setTimeout(() => {
+      console.log(`[${this.username}] Attempting to reconnect...`);
+      this.initBot();
+    }, 5000);
+  }
+}
+
+
+async function spawnBots() {
+  for (let i = 0; i < NUM_BOTS; i++) {
+    const username = `MT${i}`;
+    new MCBot(username);
+    await sleep(SPAWN_DELAY);
+  }
+}
+
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+spawnBots();
